3dbae1dc0abb23564587a3a9a637158675255c5a
Catherine Kim <abbytherabbit@gmail.com>
2019-06-23 14:30:29 -0700

    adding express and handlebars

---
+++ script.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.POST || 3030);
+
+
+//routes:
+//homepage
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+//about
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+//404 page
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+
+//500 page
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localehost:' + app.get('port') + '; press ctrl-c to terminate.');
+});
